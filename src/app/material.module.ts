653d674760bb902fda1b46d07894ba86ab3c4dc3
Eder Gael Saldaña Galván <edergaelsaldanagalvan11@gmail.com>
2025-07-03 19:28:26 -0600

    Funcionalidad del login

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from "@angular/core"
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIcon, MatIconModule } from "@angular/material/icon";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 const myModules: any = [
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule
 ];
 
 @NgModule({
--- src/app/pages/auth/auth-module.ts
+++ src/app/pages/auth/auth-module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing-module';
 import { Auth } from './auth';
+import { MaterialModule } from '../../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { Auth } from './auth';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
